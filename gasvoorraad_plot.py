21d4a809063b446f5136bc486710e8d46b78bc11
Stef Smeets <s.smeets@esciencecenter.nl>
2022-11-23 21:34:26 +0100

    Update plots

--- gasvoorraad_plot.py
+++ gasvoorraad_plot.py
@@ -11,69 +11,91 @@ except IndexError:
 
 url = "https://agsi.gie.eu/api"
 
-params = {
-    "country": "nl",
-    "from": "2022-01-01",
-    #     "to": "2022-10-21",
-    "size": 365,
-}
-
 header = {
     "x-key": TOKEN,
 }
 
-r = requests.get(url=url, params=params, headers=header)
-data = r.json()
+for year in sys.argv[1:]:
+    print(f'Processing {year}')
 
-df = pd.json_normalize(data["data"])
+    params = {
+        "country": "nl",
+        "from": f"{year}-01-01",
+        "to": f"{year}-12-31",
+        "size": 365,
+    }
 
-df["gasDayStart"] = pd.to_datetime(df["gasDayStart"])
+    r = requests.get(url=url, params=params, headers=header)
+    data = r.json()
 
-for col in (
-    "gasInStorage",
-    "consumption",
-    "consumptionFull",
-    "injection",
-    "withdrawal",
-    "netWithdrawal",
-    "workingGasVolume",
-    "injectionCapacity",
-    "withdrawalCapacity",
-    "trend",
-    "full",
-):
-    df[col] = df[col].astype(float)
+    df = pd.json_normalize(data["data"])
 
-base = (
-    alt.Chart(df)
-    .mark_line()
-    .encode(x=alt.X("gasDayStart:T", axis=alt.Axis(title="Datum")))
-)
+    df["gasDayStart"] = pd.to_datetime(df["gasDayStart"])
 
-line = base.mark_line(color="red").encode(
-    y=alt.Y(
-        "full:Q",
-        axis=alt.Axis(title="Vulniveau (%)"),
-        scale=alt.Scale(padding=0, domain=[0, 100]),
+    for col in (
+        "gasInStorage",
+        "consumption",
+        "consumptionFull",
+        "injection",
+        "withdrawal",
+        "netWithdrawal",
+        "workingGasVolume",
+        "injectionCapacity",
+        "withdrawalCapacity",
+        "trend",
+        "full",
+    ):
+        df[col] = df[col].astype(float)
+
+
+    df['withdrawal'] = -df['withdrawal']
+
+    # per_day = df.melt(id_vars=('gasDayStart',), value_vars=('injection', 'withdrawal'))
+    per_week = (df.groupby([pd.Grouper(key = 'gasDayStart', freq = 'W')])
+    .agg(injectie = ('injection' , 'sum'), afname = ('withdrawal', 'sum'))
+    .round()).melt(ignore_index=False).reset_index()
+
+    line = alt.Chart(df).mark_line(color='red').encode(
+        x=alt.X("gasDayStart:T", axis=alt.Axis(title="Datum")),
+        y=alt.Y('full:Q', 
+           axis=alt.Axis(title="Vulniveau (%)"),
+           scale=alt.Scale(padding=0, domain=[0, 100])),
+        tooltip=['gasDayStart', 'full']
     )
-)
 
-bar = base.mark_bar().encode(
-    y=alt.Y("injection", axis=alt.Axis(title="Gastoevoer (GWh/d)"))
-)
+    bar = alt.Chart(
+        per_week
+    ).mark_bar(
+        binSpacing=0, opacity=0.6
+    # ).mark_area(
+    #      interpolate='step-after', opacity=0.6, line=False,
+    ).encode(
+        x=alt.X("gasDayStart:T", 
+                axis=alt.Axis(title="Datum"),
+                scale=alt.Scale(domain=[f"{year}-01-01", f"{year}-12-31"]),
+            ),
+        y=alt.Y("value:Q", 
+                axis=alt.Axis(title="Gastoevoer (GWh/week)"), 
+                scale=alt.Scale(padding=0, domain=[-15000, 15000]),
+                stack=None,
+            ),
+        color='variable',
+        tooltip=['gasDayStart', 'variable', 'value'],
+    )
 
-chart = bar + line
+    chart = line + bar
 
-today = date.today()
+    today = date.today()
 
-chart = chart.properties(title=f"Gasvoorraad in Nederland - Geüpdated: {today}").resolve_scale(
-    y="independent"
-)
+    chart = chart.properties(title=f"Gasvoorraad in Nederland {year}").resolve_scale(
+        y="independent"
+    ).interactive()
 
-chart.save(
-    "chart.json", 
-    json_kwds={
-        'indent': 0,
-    })
+    chart.save(
+        f"chart-{year}.json", 
+        json_kwds={
+            'indent': 0,
+        })
+    chart.save(f"chart-{year}.html")
 
 # breakpoint()
